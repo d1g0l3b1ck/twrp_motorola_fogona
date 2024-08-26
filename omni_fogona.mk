047bab404cdbf423244c63862488e31c8550af83
d1g0l3b1ck <122018743+d1g0l3b1ck@users.noreply.github.com>
2024-08-26 13:06:06 -0400

    Update omni_fogona.mk

--- omni_fogona.mk
+++ omni_fogona.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from fogona device
 $(call inherit-product, device/motorola/fogona/device.mk)
 
 PRODUCT_DEVICE := fogona
-PRODUCT_NAME := omni_fogona
+PRODUCT_NAME := twrp_fogona
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g play - 2024
 PRODUCT_MANUFACTURER := motorola
