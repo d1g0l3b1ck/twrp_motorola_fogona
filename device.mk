7bd358b374236b57fd2861daa5aba0d1b53fa194
d1g0l3b1ck <122018743+d1g0l3b1ck@users.noreply.github.com>
2024-08-26 12:46:39 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.bengal
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.bengal \
     libgptutils \
     libz \
